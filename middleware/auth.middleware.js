e1520a2f50fcd481fe41abf0adc6934b600adc14
AmanKaushal982 <kaushala298@gmail.com>
2026-05-01 18:17:36 +0530

    oauth polish

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,8 +7,13 @@ const protectedRoutes = async (req, res, next) => {
         if (!token) {
             return res.status(401).json({ message: 'Not authorized, please login' })
         }
-        const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id).select('-password');
+        if (!user) {
+            return res.status(401).json({ message: 'User no loger exists' });
+        }
+        req.user = user;
+        next();
     }
     catch (err) {
         return res.status(401).json({ message: 'Token invalid or expired, please login again' });
